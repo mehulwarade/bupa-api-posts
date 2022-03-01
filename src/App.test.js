7ba70b690b235ba6a54153bbd7504bacc320472e
Mehul Warade <mehulwarade@gmail.com>
2022-03-02 00:38:52 +1100

    Start testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {shallow , mount} from 'enzyme';
+import { shallow, mount } from 'enzyme';
+
+import store from './redux/store';
 
 it('should load the original webpage', () => {
-  let wrapper = mount(<App />);
-  //console.log(wrapper.debug());
-  let findp = wrapper.find('h1').length;
-  expect(findp).toBe(1); 
+	//   let wrapper = mount(<App output={store.getState()} />);
+	wrapper = shallow(<App state={store.getState()} />)
+	//console.log(wrapper.debug());
+	let findp = wrapper.find('h1').length;
+	expect(findp).toBe(1);
 })
